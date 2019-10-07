9f1f87d3beb50653ad8d60d17f527bf02ed61b7b
JanirKillman <54068244+JanirKillman@users.noreply.github.com>
2019-10-07 20:27:46 +0200

    Add files via upload

---
+++ ConnectionFactory.vb
@@ -0,0 +1,16 @@
+﻿Imports Oracle.DataAccess.Client
+Imports System.Configuration
+
+Public Class ConnectionFactory
+
+
+    Public Shared Function GetConnection() As OracleConnection
+        Try
+            Dim connectionstring As String = ConfigurationManager.ConnectionStrings.Item("OdriaOraDB").ConnectionString
+            Dim connection As OracleConnection = New OracleConnection(connectionstring)
+            Return connection
+        Catch ex As Exception
+            MsgBox("Chyba " + ex.Message, , "Chyba")
+        End Try
+    End Function
+End Class
